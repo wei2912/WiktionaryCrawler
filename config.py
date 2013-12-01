a39021f9101e3f6cebd6f5b4507d52b117b7655e
Wei En <wei2912.supp0rt@gmail.com>
2013-12-01 15:47:31 +0800

    Change to Thai

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # coding=utf8
 
-start_cat = "Category:Mandarin language"
+start_cat = "Category:Thai parts of speech"
 api_crawl_delay = 1 # in seconds
 page_crawl_delay = 0.4 # in seconds
-lang = "zh"
+lang = "th"
 
 ## lang-specific config vals ##
 
 ## zh - Default
-zh_s = True # (true) crawl only simplified chinese
-zh_t = False # (true) crawl only traditional chinese
+zh_s = True # (true) crawl only simplified chinese -> default true
+zh_t = False # (true) crawl only traditional chinese -> default false
 
 ## lang-specific config vals ##
